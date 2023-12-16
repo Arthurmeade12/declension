deb3941cca9717de5515777f2e40f500c6f01a45
Arthurmeade12 <arthurmeade12@gmail.com>
2023-12-16 13:26:30 -0600

    Fix package.sh

--- package.sh
+++ package.sh
@@ -3,7 +3,11 @@ TARGET="${TARGET:=.}"
 JARNAME='declension.jar'
 MAINCLASS='me.arthurmeade12.decliner.declension'
 COMPILEPATH='me/arthurmeade12/decliner'
+COMPDECL=false
 javac -d "${TARGET}" "${TARGET}"/*.java
-#javac -d "${TARGET}" "${TARGET}"/**/*.java
+if [[ "${COMPDECL}" == 'true' ]]
+then
+	javac -d "${TARGET}" "${TARGET}"/**/*.java
+fi
 jar -c -f "${TARGET}/${JARNAME}" -e "${MAINCLASS}" "${TARGET}/${COMPILEPATH}"
 
