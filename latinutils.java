308385dd740110b078942cdd905ddd36bd1731d4
Arthurmeade12 <arthurmeade12@gmail.com>
2023-12-18 15:24:01 -0600

    Add support for extraline depending on cli args; get cli gender if it is given

--- latinutils.java
+++ latinutils.java
@@ -3,81 +3,167 @@ import java.util.Scanner;
 public class latinutils {
     public static boolean case_sensitive = false;
     public static byte getdecl(String nominative, String genitive) {
-        String ending = genitive.substring(genitive.length()-2);
+        String ending;
+        if (genitive.length() < 3) {
+            ending = genitive;
+        } else {
+            ending = genitive.substring(genitive.length()-2);
+        }
         if (case_sensitive = false){
             ending = ending.toLowerCase();
         }
-        byte error = 0;
+        byte ans = 0;
         switch (ending) {
         case "ae":
-            if (nominative.endsWith("a")) {
-                return 1;
-            } else {
-                error = 1;
-            }
+            ans = 1;
+            break;
         case "is":
-            return 3;
+            ans = 3;
+            break;
         case "us":
-            switch (genitive) {
-            case "ejus":
-                return 6;
-            case "cujus":
-                return 7;
-            case "hujus":
-                return 8;
-            default:
-                if (genitive.endsWith("ius")) {
-                    return 6;
-                }
-                if (nominative.endsWith("u") || nominative.endsWith("us")) {
-                    return 4;
-                } else {
-                    error = 4;
-                }
-            }
+            ans = 4;
+            break;
         case "ei":
-            if (nominative.endsWith("es")) {
-                return 5;
-            } else {
-                error = 5;
-            }
+            ans = 5;
+            break;
         default:
             if (genitive.endsWith("i")) {
-                // TODO: vir does not match these criteria
-                if (nominative.endsWith("us") || nominative.endsWith("um") || nominative.endsWith("er") || nominative.endsWith("ir")) {
-                    return 2;
-                    // will figure out base w/ or w/o e in second class
-                } else {
-                    System.out.println(nominative + " does not fall into declension #2. ");
-                    return 0;
-                }
+                ans = 2;
             } else {
-                if (error > 0) {
-                    msg.warn(nominative + " does not fall into declension #" + error + ". ");
-                } else {
-                    msg.warn(nominative + " does not fall into any declension. ");
-                }
-                return 0;
-                // 0 for errors (not falling into a declension)
+                ans = 0;
             }
+            break;
+        }
+        if (latinutils.sanenominative(nominative, ans)) {
+            return ans;
+        } else {
+            msg.warn(nominative + " does not fall into any declension. ");
+            return 0;
         }
     }
-    public static String getbase(String genitive, byte decl) {
+    public static boolean sanenominative(String nominative, byte decl) {
+        boolean value = false;
         switch (decl) {
         case 1:
-        case 3:
-        case 4:
-        case 5:
-            return genitive.substring(0, genitive.length() - 2);
+            if (nominative.endsWith("a")) {
+                value = true;
+            }
+            break;
         case 2:
-            return genitive.substring(0, genitive.length() - 1);
-        case 6:
-            return genitive.substring(0, genitive.length() - 3);
-        case 7:
-        case 8:
-            return "";
+            switch (nominative.substring(nominative.length() - 2)) {
+            case "us":
+            case "um":
+            case "er":
+            case "ir":
+                value = true;
+            }
+            break;
+        case 3:
+            value =  true;
+        case 4:
+            if (nominative.endsWith("u") || nominative.endsWith("us")) {
+                value = true;
+            }
+            break;
+        case 5:
+            if (nominative.endsWith("es")) {
+                value = true;
+            }
+            break;
         default:
-            return "0";
+            value = false;
+        }
+        msg.debug("Sane nominative: " + value);
+        return value;
+    }
+    public static String getbase(String nominative, String genitive, byte decl) {
+        if (decl == 0) { // fail hard immediately to avoid repeating work
+            return "";
+        }
+        if (genitive.length() > 3) { // prefer getting base from genitive; it's proper
+            msg.debug("Detected full genitive.");
+            switch (decl) {
+            case 1:
+            case 3:
+            case 4:
+            case 5:
+                return genitive.substring(0, genitive.length() - 2);
+            case 2:
+                return genitive.substring(0, genitive.length() -    1);
+            default:
+                return "";
+            }
+        } else {
+            return latinutils.buildbase(nominative, decl);
+        }
+    }
+    public static String buildbase(String nominative, byte decl) { // go from nominative if we have to
+        msg.debug("Building a genitive (only ending was given)");
+        String minusone = nominative.substring(0, nominative.length() - 1);
+        String minustwo = nominative.substring(0, nominative.length() - 2); // cache these since we use them so often
+        switch (decl) {
+        case 1:
+            return minusone;
+        case 2:
+            switch (nominative.substring(nominative.length() - 2)) {
+            case "us":
+            case "um":
+                return minustwo;
+            case "ir":
+                return nominative;
+            case "er":
+                msg.out("Is the genitive of " + nominative + " " + nominative + "i or " + minustwo + "ri ? ");
+                msg.out ("Answer 1 for the first, 2 for the second.");
+                Scanner gendersecond = new Scanner(System.in);
+                String ans; // make string to prevent user from crashing the app
+                for (byte i = 0; i < 3; i++) {
+                    ans = gendersecond.next();
+                    switch (ans) {
+                    case "1":
+                        return nominative;
+                    case "2":
+                        return minustwo + "r";
+                    default:
+                        msg.warn("Answer was not 1 or 2.");
+                    }
+                }
+                break;
+            }
+            msg.die("Three incorrect attempts. ", 2);
+            return "";
+        case 3:
+            //TODO Attempt to resolve genitive automatically
+            msg.out("You must enter the full genitive for third declension.");
+            msg.out("Please enter the full genitive : ");
+            Scanner genitivethird = new Scanner(System.in);
+            String ans;
+            for (byte i = 0; i < 3; i++) {
+                ans = genitivethird.next();
+                if (ans.endsWith("is") && ans.length() > 2) {
+                    return ans.substring(0, ans.length() -2);
+                } else {
+                    msg.warn("The entered answer does not conform to the third declension (-is) and/or is not long enough.");
+                }
+            }
+            msg.die("Three incorrect attempts. ", 2);
+            return "";
+        case 4:
+            if (nominative.endsWith("u")) {
+                return minusone;
+            } else { // endsWith("us")
+                return minustwo;
+            }
+        case 5:
+            return minustwo;
+        default:
+            return "";
+        }
+    }
+    private static boolean nomgenpair(String nominative, String nomending, String genitive, String genending) { // exclusively used in determining gender of thirds
+        if (nominative.endsWith(nomending) && genitive.endsWith(genending)) {
+            return true;
+        } else {
+            return false;
         }
     }
     public static char getgender(String nominative, String genitive, byte decl) {
@@ -114,17 +200,17 @@ public class latinutils {
             }
         case 3:
             // here we go (!!)
-            if (nominative.endsWith("mare") || (nominative.endsWith("men") && genitive.endsWith("minis")) || (nominative.endsWith("us") && genitive.endsWith("oris"))) {
+            if (nominative.endsWith("mare") || nomgenpair(nominative, "men", genitive, "minis") || nomgenpair(nominative, "us", genitive, "oris")) {
                 return 'n';
-            } else if ((nominative.endsWith("tas") && genitive.endsWith("tatis")) || (nominative.endsWith("tus") && genitive.endsWith("tudis"))) {
+            } else if (nomgenpair(nominative, "tas", genitive, "tatis") || nomgenpair(nominative, "tus", genitive, "tudis")) {
                 return 'f';
             } else {
                 msg.out("What is the gender of " + nominative + " ?");
                 msg.out("m for masculine, f for feminine, n for neuter : ");
-                Scanner genthird = new Scanner(System.in);
+                Scanner genderthird = new Scanner(System.in);
                 char ans = ' ';
-                for (byte i = 1; i > 0; i++) {
-                    ans = genthird.next().charAt(0);
+                for (byte i = 0; i < 3; i++) {
+                    ans = genderthird.next().charAt(0);
                     switch (ans) {
                     case 'm':
                     case 'f':
@@ -135,14 +221,11 @@ public class latinutils {
                     case 'N':
                         return Character.toLowerCase(ans);
                     default:
-                        if (i == 3) {
-                            msg.die("Three incorrect attempts. ", 2);
-                        } else {
-                            msg.warn("Invalid answer. Choose m, f, or n");
-                        }
+                        msg.warn("Answer was not of m, f, or n.");
                     }
                 }
-                return ans;
+                msg.die("Three incorrect attempts. ", 2);
+                return '0';
             }
         case 4:
             if (nominative.endsWith("u")) {
@@ -170,16 +253,6 @@ public class latinutils {
                 return 'm';
             } else {
                 return 'f';
-            }
-        case 8:
-            // hic haec hoc
-            switch (nominative) {
-            case "hic":
-                return 'm';
-            case "haec":
-                return 'f';
-            case "hoc":
-                return 'n';
             }
         default:
             return '0';
